8508e0ebec5dc9d6eb23fdc8ae0bfafdbca28b3d
21givenchy <georgekarani44@gmail.com>
2024-01-23 18:51:14 +0300

    location

--- dispatch-main/src/components/Form.jsx
+++ dispatch-main/src/components/Form.jsx
@@ -1,17 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
-// TODO: style form
-// TODO: add labels
-// TODO: add inputs
-
-export default function Form() {
+function Form() {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const onSubmit = (data) => console.log(data);
   console.log(errors);
+
+  const [truckLocation, setTruckLocation] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://gps51.com/#/tracking?isshare=1&deviceid=9172808375&authcode=101b81accc1b2ffe6ab1788f25c09387&devicename=KCV645L&language=zh')
+      .then(res => res.json())
+      .then(data => setTruckLocation(data.features[0].center))
+  }, []);
+
+  console.log(truckLocation);
 
   return (
     <form
@@ -24,35 +32,13 @@ export default function Form() {
         placeholder="First name"
         {...register('First name', { required: true, maxLength: 80 })}
       />
-      <input
-        className="border-[1px] rounded-lg border-[#27272a] bg-transparent py-2 px-3 text-sm"
-        type="text"
-        placeholder="Last name"
-        {...register('Last name', { required: true, maxLength: 100 })}
-      />
-      <input
-        className="border-[1px] rounded-lg border-[#27272a] bg-transparent py-2 px-3 text-sm"
-        type="text"
-        placeholder="Email"
-        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <input
-        className="border-[1px] rounded-lg border-[#27272a] bg-transparent py-2 px-3 text-sm"
-        type="tel"
-        placeholder="Mobile number"
-        {...register('Mobile number', {
-          required: true,
-          minLength: 6,
-          maxLength: 12,
-        })}
-      />
-
-      <button className="self-start mt-3">
-        <input
-          className="rounded-lg bg-neutral-50 text-[#18181B] py-2 px-4 text-sm cursor-pointer"
-          type="submit"
-        />
-      </button>
+      <div>
+        Truck Location: {truckLocation ? `${truckLocation[0]}, ${truckLocation[1]}` : 'Loading...'}
+      </div>
+      
+      <input type="submit" />
     </form>
   );
 }
+
+export default Form;
