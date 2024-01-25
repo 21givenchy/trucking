1fc98b79be4d2f8b35fe9f741e1adbb27297ed08
21givenchy <georgekarani44@gmail.com>
2024-01-25 12:15:32 +0300

    security patch

--- dispatch-main/src/components/Map.jsx
+++ dispatch-main/src/components/Map.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Map = () => (
   <iframe 
-    src="http://gps51.com/#/tracking?isshare=1&deviceid=9172808375&authcode=a07474a4fd5c5d33037efa1e818c0f9f&devicename=KCV645L&language=zh" 
-    style={{ height: "500px", width: "100%" }}
-    title="Embedded Map"
+  src="https://gps51.com/#/tracking?isshare=1&deviceid=9172808375&authcode=a07474a4fd5c5d33037efa1e818c0f9f&devicename=KCV645L&language=zh" 
+  style={{ height: "500px", width: "100%" }}
+  title="Embedded Map"
   />
 );
 
